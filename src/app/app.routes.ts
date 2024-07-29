6c88fd50fe78871176648c24fd2303a614bb527b
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-07-28 21:59:06 -0300

    making routes, views/scaffolding

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
+    },
+    {
+        path: 'trips',
+        loadChildren: () => import('./trips/trips.routes').then(m => m.TRIPS_ROUTES)
+    }
+
+];
