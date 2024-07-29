74b53d4ca749b9f988621951958f4f54f1a4f6bf
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-07-29 00:17:00 -0300

    making schema for home view

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
     {
-        path: '',
+        path: '', component: HomeComponent
+    },
+    {
+        path: 'auth',
         loadChildren: () => import('./auth/auth.routes').then(m => m.AUTH_ROUTES)
     },
     {
