a8845664a03168af0c0e7a05963df9dc4a39af06
AngelGabrielG <angel.gaitan.ev@gmail.com>
2024-07-29 23:14:03 -0300

    card component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { MainComponent } from './components/main/main.component';
+import { BottomSectionComponent } from './components/bottom-section/bottom-section.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [MainComponent],
+  imports: [MainComponent, BottomSectionComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+
+  scrollToTop() {
+    window.scrollTo(0, 0);
+  }
 
 }
